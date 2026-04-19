336b1821c1191c3436293e295e8c93c144960f34
Mate Javakhadze <matejavakhadze0112@gmail.com>
2026-04-19 16:30:04 +0400

    Initial Commit

---
+++ MapOut/MapOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapOutApp.swift
+//  MapOut
+//
+//  Created by Mate Javakhadze on 19.04.26.
+//
+
+import SwiftUI
+
+@main
+struct MapOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
